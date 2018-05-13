daf68196f2a34ae36b58a503b1ee42959827aee0
smulionOong <1224police@likelion.org>
2018-05-14 00:01:14 +0900

    lcs 다이나믹 버전 숫자+문자까지

--- algorithm_lcs/algorithm_lcs/lcs_dp_test.cpp
+++ algorithm_lcs/algorithm_lcs/lcs_dp_test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdio>
 #include <string>
+#include <stack>
 
 using namespace std;
 
@@ -55,8 +56,44 @@ int main()
 	}
 	*/
 
+
+	int i = len1 - 1;
+	int j = len2 - 1;
+	stack<int> st; // 거꾸로 담기니 stack을 이용
+
+	while (lcs[i][j] != 0)
+	{
+		// 경로 추적
+		// cout << " i :: " << i << " j :: " << j << endl;
+
+		// 테이블이 같은 넘버링이라면
+		// 왼쪽으로 이동
+		if (lcs[i][j] == lcs[i][j - 1])
+			j--;
+
+		// 위쪽으로 이동
+		else if (lcs[i][j] == lcs[i - 1][j])
+			i--;
+
+		// 왼쪽 위쪽 모두 다른 넘버링이라면 대각선 방향으로 이동
+		else if (lcs[i][j] - 1 == lcs[i - 1][j - 1])
+		{
+			st.push(i);
+			i--;
+			j--;
+		}
+	}
+
+	// 길이 출력
 	cout << lcs[len1 - 1][len2 - 1] << endl;
 
+	// 단어 출력
+	while (!st.empty())
+	{
+		cout << str1[st.top()];
+		st.pop();
+	}
 	return 0;
 }
+
 
